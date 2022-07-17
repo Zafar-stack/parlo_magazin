856bb1eb4a17ca84bdb8ed0cad05f67188217b16
zafar <rozikulov02@gmail.com>
2022-07-17 22:32:37 +0600

    8-dars

--- main/views.py
+++ main/views.py
@@ -29,9 +29,42 @@ def catalogItemHandler(request, catalog_id):
     sizes = Size.objects.filter(category__id=catalog_id)
     tags = Tag.objects.all()
     search_value = request.GET.get('q', None)
+    active_brands = request.GET.getlist('active_brand', [])
+    active_brands = [int(i) for i in active_brands]
+
+    active_colors = request.GET.getlist('active_color', [])
+    active_colors = [int(i) for i in active_colors]
+
+    active_sizes = request.GET.getlist('active_size', [])
+    active_sizes = [int(i) for i in active_sizes]
 
     if search_value:
-        goods = Good.objects.filter(category__id=catalog_id).filter(title__contains=search_value)
+        new_goods = []
+        for g in goods:
+            if g.title.find(search_value) > -1:
+                new_goods.append(g)
+        goods = new_goods
+
+    if active_brands:
+        new_goods = []
+        for g in goods:
+            if g.brand and str(g.brand.id) in active_brands:
+                new_goods.append(g)
+        goods = new_goods
+
+    if active_colors:
+        new_goods = []
+        for g in goods:
+            if g.color and g.color.id in active_brands:
+                new_goods.append(g)
+        goods = new_goods
+
+    if active_sizes:
+        new_goods = []
+        for g in goods:
+            if g.size and g.size.id in active_sizes:
+                new_goods.append(g)
+        goods = new_goods
 
     return render(request, 'catalog.html', {'categories': categories,
                                             'goods': goods,
@@ -41,6 +74,9 @@ def catalogItemHandler(request, catalog_id):
                                             'tags': tags,
                                             'active_category': active_category,
                                             'search_value': search_value,
+                                            'active_brands': active_brands,
+                                            'active_colors': active_colors,
+                                            'active_sizes': active_sizes,
                                             })
 
 
