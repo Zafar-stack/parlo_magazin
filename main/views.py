9c0b1fcdad1fdcd887308aa83038292a8e9a5588
zafar <rozikulov02@gmail.com>
2022-07-19 19:15:51 +0600

    9-dars

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from main.models import *
+from math import ceil
 
 # Create your views here.
 
@@ -29,6 +30,7 @@ def catalogItemHandler(request, catalog_id):
     sizes = Size.objects.filter(category__id=catalog_id)
     tags = Tag.objects.all()
     search_value = request.GET.get('q', None)
+
     active_brands = request.GET.getlist('active_brand', [])
     active_brands = [int(i) for i in active_brands]
 
@@ -37,6 +39,16 @@ def catalogItemHandler(request, catalog_id):
 
     active_sizes = request.GET.getlist('active_size', [])
     active_sizes = [int(i) for i in active_sizes]
+
+    is_discount = request.GET.get('is_discount', None)
+    is_new = request.GET.get('is_new', None)
+    stock = request.GET.get('stock', None)
+
+    price = request.GET.get('price', None)
+    price_start, price_stop = None, None
+    if price and len(price.split('-')) == 2:
+        price_start = int(price.split('-')[0])
+        price_stop = int(price.split('-')[1])
 
     if search_value:
         new_goods = []
@@ -48,14 +60,14 @@ def catalogItemHandler(request, catalog_id):
     if active_brands:
         new_goods = []
         for g in goods:
-            if g.brand and str(g.brand.id) in active_brands:
+            if g.brand and g.brand.id in active_brands:
                 new_goods.append(g)
         goods = new_goods
 
     if active_colors:
         new_goods = []
         for g in goods:
-            if g.color and g.color.id in active_brands:
+            if g.color and g.color.id in active_colors:
                 new_goods.append(g)
         goods = new_goods
 
@@ -66,8 +78,48 @@ def catalogItemHandler(request, catalog_id):
                 new_goods.append(g)
         goods = new_goods
 
+    if is_discount:
+        new_goods = []
+        for g in goods:
+            if g.is_discount:
+                new_goods.append(g)
+        goods = new_goods
+
+    if is_new:
+        new_goods = []
+        for g in goods:
+            if g.is_new:
+                new_goods.append(g)
+        goods = new_goods
+
+    if stock:
+        new_goods = []
+        for g in goods:
+            if g.stock > 0:
+                new_goods.append(g)
+        goods = new_goods
+
+    if price_start and price_stop:
+        new_goods = []
+        for g in goods:
+            if g.price >= price_start and g.price <= price_stop:
+                new_goods.append(g)
+        goods = new_goods
+
+    limit = request.GET.get('limit', 9)
+    current_page = int(request.GET.get('page', 0))
+    total = len(goods)
+    pages_count = ceil(total / limit)
+    pages = range(1, pages_count + 1)
+    stop = current_page * limit
+    start = stop - limit
+    prev_page = current_page - 1
+    next_page = None
+    if current_page < pages_count:
+        next_page = current_page + 1
+
     return render(request, 'catalog.html', {'categories': categories,
-                                            'goods': goods,
+                                            'goods': goods[start:stop],
                                             'brands': brands,
                                             'colors': colors,
                                             'sizes': sizes,
@@ -77,6 +129,17 @@ def catalogItemHandler(request, catalog_id):
                                             'active_brands': active_brands,
                                             'active_colors': active_colors,
                                             'active_sizes': active_sizes,
+                                            'is_discount': is_discount,
+                                            'is_new': is_new,
+                                            'stock': stock,
+                                            'price': price,
+                                            'pages': pages,
+                                            'current_page': current_page,
+                                            'prev_page': prev_page,
+                                            'next_page': next_page,
+                                            'total': total,
+                                            'start': start,
+                                            'stop': stop,
                                             })
 
 
